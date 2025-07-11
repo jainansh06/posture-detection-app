808d497b640715cb30946e350e6e0953b0cebd30
Ansh Jain <jainansh0610@gmail.com>
2025-07-11 09:18:41 +0530

    Update gunicorn_config.py

--- backend/gunicorn_config.py
+++ backend/gunicorn_config.py
@@ -1,4 +1,7 @@
-bind = "0.0.0.0:5000"
+import os
+
+#bind = "0.0.0.0:5000" aws
+bind = f"0.0.0.0:{os.environ.get('PORT', '5000')}"
 workers = 2
 worker_class = "sync"
 worker_connections = 1000
@@ -6,3 +9,7 @@ timeout = 300
 keepalive = 5
 max_requests = 1000
 max_requests_jitter = 100
+
+# Additional settings for production (not aws)
+preload_app = True
+worker_tmp_dir = "/dev/shm"
